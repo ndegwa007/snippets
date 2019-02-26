915a7ddd8bb1a10a3b39da33c2eb73ca9f052842
abc <abc@example.com>
2019-02-26 19:57:55 +0300

    tribonacci series

---
+++ tribo.py
@@ -0,0 +1,27 @@
+'''tribonacci series'''
+
+
+
+def tribonacci(s, n):
+
+    n1 = s[0]
+    n2 = s[1]
+    n3 = s[2]
+  
+    count = 0
+    if n == 0:
+        print[]
+    elif n == 1:
+        print[1]
+    else:
+        while count < n:
+            print(n1, end=' , ')
+            nth = n1 + n2 + n3
+            #update values
+            n1 = n2
+            n2 = n3
+            n3 = nth
+            count += 1
+        
+            
+
